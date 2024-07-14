5e94cf70672cef2fb60834eb0dbc26b9cafd9a70
songseongbin <ppsosqq@gmail.com>
2024-07-14 20:43:12 +0900

    init commit

---
+++ src/main/java/tobyspring/spring6/TobySpring6Application.java
@@ -0,0 +1,13 @@
+package tobyspring.spring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpring6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobySpring6Application.class, args);
+	}
+
+}
---
+++ src/test/java/tobyspring/spring6/TobySpring6ApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.spring6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpring6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
