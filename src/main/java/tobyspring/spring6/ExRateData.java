efe6323715e98af3f65ac1a2b3619150a73e8db2
songseongbin <ppsosqq@gmail.com>
2024-07-16 23:21:37 +0900

    paymentService 구현3
    paymentService 구현3

---
+++ src/main/java/tobyspring/spring6/ExRateData.java
@@ -0,0 +1,10 @@
+package tobyspring.spring6;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
