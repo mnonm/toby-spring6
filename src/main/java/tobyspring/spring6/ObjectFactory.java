be0ea1d27b6abc49fa2275d345e0c082fe9171d5
songseongbin <ppsosqq@gmail.com>
2024-07-28 22:52:33 +0900

    [refactor] 구성정보를 가져오는 다른 방법

--- src/main/java/tobyspring/spring6/ObjectFactory.java
+++ src/main/java/tobyspring/spring6/ObjectFactory.java
@@ -1,17 +1,19 @@
 package tobyspring.spring6;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-	@Bean
-	public PaymentService paymentService() {
-		return new PaymentService(exRateProvider());
-	}
-
-	@Bean
-	public ExRateProvider exRateProvider() {
-		return new WebApiExRateProvider();
-	}
+	// @Bean
+	// public PaymentService paymentService() {
+	// 	return new PaymentService(exRateProvider());
+	// }
+	//
+	// @Bean
+	// public ExRateProvider exRateProvider() {
+	// 	return new WebApiExRateProvider();
+	// }
 }
