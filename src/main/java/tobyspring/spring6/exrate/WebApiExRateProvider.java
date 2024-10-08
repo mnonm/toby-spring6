1ac39ce4577bdda794f81aadf0ee941fcf0aedd6
songseongbin <ppsosqq@gmail.com>
2024-10-08 11:40:31 +0900

    [refactor] ApiExecutor 분리 - 인터페이스 도입과 클래스 분리

--- src/main/java/tobyspring/spring6/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/spring6/exrate/WebApiExRateProvider.java
@@ -1,19 +1,16 @@
 package tobyspring.spring6.exrate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import tobyspring.spring6.api.SimpleApiExecutor;
 import tobyspring.spring6.payment.ExRateProvider;
 
 @Component
@@ -35,7 +32,7 @@ public class WebApiExRateProvider implements ExRateProvider {
 
 		String response;
 		try {
-			response = executeApi(uri);
+			response = new SimpleApiExecutor().execute(uri);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -51,14 +48,5 @@ public class WebApiExRateProvider implements ExRateProvider {
 		ObjectMapper mapper = new ObjectMapper();
 		ExRateData data = mapper.readValue(response, ExRateData.class);
 		return data.rates().get("KRW");
-	}
-
-	private static String executeApi(URI uri) throws IOException {
-		String response;
-		HttpURLConnection connection = (HttpURLConnection)uri.toURL().openConnection();
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-			response = br.lines().collect(Collectors.joining());
-		}
-		return response;
 	}
 }
