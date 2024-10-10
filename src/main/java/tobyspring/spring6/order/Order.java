95d0ba88b7ee89a8ad1b8da42061ad7b875bd5cf
songseongbin <ppsosqq@gmail.com>
2024-10-10 21:04:28 +0900

    [refactor] Order entity jpa 설정 xml로 변경

--- src/main/java/tobyspring/spring6/order/Order.java
+++ src/main/java/tobyspring/spring6/order/Order.java
@@ -2,19 +2,9 @@ package tobyspring.spring6.order;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "orders")
 public class Order {
-	@Id @GeneratedValue
 	private Long id;
 
-	@Column(unique = true)
 	private String no;
 
 	private BigDecimal total;
@@ -25,6 +15,18 @@ public class Order {
 	public Order(String no, BigDecimal total) {
 		this.no = no;
 		this.total = total;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNo() {
+		return no;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
 	}
 
 	@Override
