3319ec93f05ba917a802985b254d020a9e26d748
songseongbin <ppsosqq@gmail.com>
2024-10-10 11:57:22 +0900

    [feat] DataAccessException과 예외 추상화

--- src/main/java/tobyspring/spring6/DataConfig.java
+++ src/main/java/tobyspring/spring6/DataConfig.java
@@ -2,11 +2,14 @@ package tobyspring.spring6;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
@@ -15,13 +18,12 @@ import tobyspring.spring6.data.OrderRepository;
 
 @Configuration
 public class DataConfig {
-	// data source
+
 	@Bean
 	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
 	}
 
-	// entity manager factory
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
@@ -37,7 +39,17 @@ public class DataConfig {
 	}
 
 	@Bean
-	public OrderRepository orderRepository(EntityManagerFactory emf) {
-		return new OrderRepository(emf);
+	public BeanPostProcessor persistenceAnnotationBeanPostProcessor() {
+		return new PersistenceAnnotationBeanPostProcessor();
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+
+	@Bean
+	public OrderRepository orderRepository() {
+		return new OrderRepository();
 	}
 }
--- src/main/java/tobyspring/spring6/data/OrderRepository.java
+++ src/main/java/tobyspring/spring6/data/OrderRepository.java
@@ -1,37 +1,15 @@
 package tobyspring.spring6.data;
 
-import java.math.BigDecimal;
-
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import tobyspring.spring6.order.Order;
 
 public class OrderRepository {
 
-	private final EntityManagerFactory emf;
-
-	public OrderRepository(EntityManagerFactory emf) {
-		this.emf = emf;
-	}
+	@PersistenceContext
+	private EntityManager em;
 
 	public void save(Order order) {
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction transaction = em.getTransaction();
-		transaction.begin();
-
-		try {
-			em.persist(order);
-			em.flush();
-
-			transaction.commit();
-		} catch (RuntimeException e) {
-			if (transaction.isActive())
-				transaction.rollback();
-			throw e;
-		} finally {
-			if (em.isOpen())
-				em.close();
-		}
+		em.persist(order);
 	}
 }
